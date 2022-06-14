d19c989537cd8092602f9f8c70cdd661017bf1ca
Chetan Chavan <chetan1792000@gmail.com>
2022-06-14 17:07:50 +0530

    added changes

---
+++ DataDriven1/src/main/java/com/selenium/handson/DataDriven1/DataTest.java
@@ -0,0 +1,18 @@
+package com.selenium.handson.DataDriven1;
+
+import java.util.ArrayList;
+
+public class DataTest {
+
+	//
+	
+	public ArrayList<String> getData(String testCaseName){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
